ffd66c2e4a5c939ceb691e4f3927906f0c82dcfe
Kihwan Kim <liamgall@naver.com>
2016-08-22 04:15:23 +0900

    Update textediter.js

--- pages/textediter/textediter.js
+++ pages/textediter/textediter.js
@@ -1,24 +1,38 @@
+const http = require('http');
+const express = require('express');
 const mongo = require('mongodb');
+const bodyparser = require('body-parser');
+
+var path = require('path');
 var MongoClient = mongo.MongoClient;
+var expressapp = express();
+expressapp.use(bodyparser.urlencoded({extended:true}));
+expressapp.use(express.static(path.join(__dirname, 'public')));
+var router = express.Router();
+const hostname = '127.0.0.1';
+const port = 3000;
 
+router.use(function(req, res, next) {
+	console.log(req.method, req.url);
+	next();
+});
 
+http.createServer(expressapp).listen(3000);
+
+expressapp.get('/', function(req, res){
+	console.log("aa");
+});
 
 function startdb(){
-	//code from 'ENOW code editor'
-	var code = document.getElementById("iframecode").contentWindow['code'];
-	//name of sourcecode saved in mongodb
-    var name = document.getElementById('nameofcode');
-	//connect to mongodb. insert data in 'source' db
-    MongoClient.connect('mongodb://localhost/source',function(err,db) {
-        var src = document.getElementById('source');
-        var name = document.getElementById('nameofcode');
+    var name = document.getElementById('src');
+    console.log(name.value);
+    MongoClient.connect('mongodb://localh	ost/source',function(err,db) {
+        var i = document.getElementById('src');
         console.log("start db");
-		//insert in mongodb.
         var insertDocument = function(db, callback){
-			//insert in 'codes' collection
             db.collection('codes').insertOne({
-                "name" : name.value,
-                "source" : code
+                "name" : "hello",
+                "source" : i.value
             },function(err, result){
                 console.log("Inserted a document into logs collection.");
                 callback();
