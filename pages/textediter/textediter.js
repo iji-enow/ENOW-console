1227f2f7558c1e097f36a595db613cf80cff4b67
Kihwan Kim <liamgall@naver.com>
2016-08-23 03:35:58 +0900

    update mongodb

--- pages/textediter/textediter.js
+++ pages/textediter/textediter.js
@@ -6,33 +6,39 @@ const bodyparser = require('body-parser');
 var path = require('path');
 var MongoClient = mongo.MongoClient;
 var expressapp = express();
+
 expressapp.use(bodyparser.urlencoded({extended:true}));
-expressapp.use(express.static(path.join(__dirname, 'public')));
-var router = express.Router();
+expressapp.use(express.static(path.join(__dirname+"/../../../", 'dashboard')));
 const hostname = '127.0.0.1';
 const port = 3000;
 
-router.use(function(req, res, next) {
-	console.log(req.method, req.url);
-	next();
-});
-
-http.createServer(expressapp).listen(3000);
+// http.createServer(expressapp).listen(port);
+// server.listen(port);
+// var server = http.createServer(function(req, res){
+expressapp.set('port', port);
 
 expressapp.get('/', function(req, res){
-	console.log("aa");
+	res.sendFile("./index.html");
+	console.log("zzzz");
 });
 
-function startdb(){
-    var name = document.getElementById('src');
-    console.log(name.value);
-    MongoClient.connect('mongodb://localh	ost/source',function(err,db) {
-        var i = document.getElementById('src');
+expressapp.post('/', function(req, res){
+	console.log(req.param('name',null));
+	startdb(req.param('name',null));
+});
+
+var server = expressapp.listen(expressapp.get('port'), function(){
+  console.log('Magic happens on ' + __dirname);
+});
+
+
+function startdb(source){
+    MongoClient.connect('mongodb://localhost/source',function(err,db) {
         console.log("start db");
         var insertDocument = function(db, callback){
             db.collection('codes').insertOne({
                 "name" : "hello",
-                "source" : i.value
+                "source" : source
             },function(err, result){
                 console.log("Inserted a document into logs collection.");
                 callback();
