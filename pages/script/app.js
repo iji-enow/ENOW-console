58c15adb980ce25411ffb95ebf8294f6b7bc947e
liamgall <liamgall@naver.com>
2016-09-07 21:31:55 +0900

    ing

--- pages/script/app.js
+++ pages/script/app.js
@@ -1,30 +1,61 @@
 
-var app = angular.module('mainModule', ['ngDragDrop']);
+var app = angular.module('mainModule', ['ngDragDrop','gridster']);
 // var app = angular.module('mainModule',[]);
-
 app.controller('myCtrl', function($scope, $http){
-  $scope.list1 = [];
-  $scope.list2 = [];
-  $scope.list3 = [];
-  $scope.list4 = [];
-  $scope.list5 = [
-      {"deviceId":"dev1"},
-      {"deviceId":"dev2"},
-      {"deviceId":"dev3"},
-      {"deviceId":"dev4"},
-      {"deviceId":"dev5"},
-      {"deviceId":"dev5"},
-      {"deviceId":"dev5"},
-      {"deviceId":"dev5"},
-      {"deviceId":"dev5"},
-      {"deviceId":"dev5"},
-      {"deviceId":"dev5"},
-      {"deviceId":"dev5"},
-      {"deviceId":"dev5"},
-      {"deviceId":"dev6"}
-  ];
-  $scope.name = "kihwan";
-  $http.get("/get_db").then(function(response, error){
-      $scope.list5 = response.data;
-  })
+    $scope.list5 = [
+        {"deviceId":"dev1"},
+        {"deviceId":"dev2"},
+        {"deviceId":"dev3"},
+        {"deviceId":"dev4"},
+        {"deviceId":"dev5"},
+        {"deviceId":"dev5"},
+        {"deviceId":"dev5"},
+        {"deviceId":"dev5"},
+        {"deviceId":"dev5"},
+        {"deviceId":"dev5"},
+        {"deviceId":"dev5"},
+        {"deviceId":"dev5"},
+        {"deviceId":"dev5"},
+        {"deviceId":"dev6"}
+    ];
+    $scope.name = "kihwan";
+    $http.get("/get_db").then(function(response, error){
+        $scope.list5 = response.data;
+    })
+    $scope.maplist = [
+        {"deviceId":"dev1"},
+        {"deviceId":"dev2"},
+        {"deviceId":"dev3"},
+        {"deviceId":"dev4"}
+    ];
+    $scope.gridsterOpts = {
+        minRows: 2, // the minimum height of the grid, in rows
+        maxRows: 100,
+        columns: 6, // the width of the grid, in columns
+        colWidth: 'auto', // can be an integer or 'auto'.  'auto' uses the pixel width of the element divided by 'columns'
+        rowHeight: 'match', // can be an integer or 'match'.  Match uses the colWidth, giving you square widgets.
+        margins: [10, 10], // the pixel distance between each widget
+        defaultSizeX: 2, // the default width of a gridster item, if not specifed
+        defaultSizeY: 1, // the default height of a gridster item, if not specified
+        mobileBreakPoint: 600, // if the screen is not wider that this, remove the grid layout and stack the items
+        resizable: {
+            enabled: true,
+            start: function (event, uiWidget, $element) {
+            }, // optional callback fired when resize is started,
+            resize: function (event, uiWidget, $element) {
+            }, // optional callback fired when item is resized,
+            stop: function (event, uiWidget, $element) {
+            } // optional callback fired when item is finished resizing
+        },
+        draggable: {
+            enabled: true, // whether dragging items is supported
+            handle: '.ddd', // optional selector for resize handle
+            start: function (event, uiWidget, $element) {
+            }, // optional callback fired when drag is started,
+            drag: function (event, uiWidget, $element) {
+            }, // optional callback fired when item is moved,
+            stop: function (event, uiWidget, $element) {
+            } // optional callback fired when item is finished dragging
+        }
+    };
 });
